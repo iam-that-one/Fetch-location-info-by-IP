141b1353d37e4fcacdf5e2b84a551c7e025b772a
iam-that-one <iam-that-one@users.noreply.github.com>
2021-04-13 23:24:05 +0300

    Initial Commit

---
+++ Find-location-by-IP/Find_location_by_IPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_location_by_IPApp.swift
+//  Find-location-by-IP
+//
+//  Created by Abdullah Alnutayfi on 13/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Find_location_by_IPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
